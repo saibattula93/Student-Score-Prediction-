c0251a88808995d02ede675e53e2a1c75b148414
saibattula93 <saidurgaprasadbattula@gmail.com>
2024-01-30 23:30:36 +0530

    Data transformation

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from src.student_score_prediction.logger import logging
 from src.student_score_prediction.exception import CustomException
 from src.student_score_prediction.components.data_ingestion import DataIngestion
 from src.student_score_prediction.components.data_ingestion import DataIngestionConfig
+from src.student_score_prediction.components.data_transformation import DataTransformationConfig,DataTransformation
 import sys
 
 if __name__ == '__main__':
@@ -10,7 +11,10 @@ if __name__ == '__main__':
     try:
         #data_ingestion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path = data_ingestion.initiate_data_ingestion()
+
+        data_transformation = DataTransformation()
+        data_transformation.initiate_data_transormation(train_data_path,test_data_path)
         
     except Exception as e:
         logging.info("Custom Exception")
