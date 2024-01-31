47f76a773d6cb804f983ad877da0633bd54cd356
saibattula93 <saidurgaprasadbattula@gmail.com>
2024-01-31 23:29:48 +0530

    Model Trainer & Hyperparameter tuning

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import sys
 from src.student_score_prediction.logger import logging
 from src.student_score_prediction.exception import CustomException
 from src.student_score_prediction.components.data_ingestion import DataIngestion
 from src.student_score_prediction.components.data_ingestion import DataIngestionConfig
 from src.student_score_prediction.components.data_transformation import DataTransformationConfig,DataTransformation
-import sys
+from src.student_score_prediction.components.model_tranier import ModelTrainerConfig,ModelTrainer
+
+
 
 if __name__ == '__main__':
     logging.info("The execution has started")
@@ -14,7 +17,11 @@ if __name__ == '__main__':
         train_data_path,test_data_path = data_ingestion.initiate_data_ingestion()
 
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
         
     except Exception as e:
         logging.info("Custom Exception")
